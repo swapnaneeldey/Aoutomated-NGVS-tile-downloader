f784e681d0fe052421e886a131b1912b7a89c1c2
swapnaneeldey <89551961+swapnaneeldey@users.noreply.github.com>
2024-04-11 13:05:28 -0700

    Update fits_download.py
    Download NGVS tiles fast

--- fits_download.py
+++ fits_download.py
@@ -6,21 +6,22 @@ import time
 base_url = 'https://ws.cadc-ccda.hia-iha.nrc-cnrc.gc.ca/raven/files/cadc:CFHTSG/'
 
 # Get the input from the user
-for tile_id in []:##"NGVS+0-7", "NGVS-1-7","NGVS-2-7", "NGVS+1-6", "NGVS+0-6", "NGVS-1-6", "NGVS-2-6", "NGVS-3-6", "NGVS+5+2"
+for tile_id in ['NGVSm2p0'] : #["NGVS+0-7", "NGVS-1-7","NGVS-2-7", "NGVS+1-6", "NGVS+0-6", "NGVS-1-6", "NGVS-2-6", "NGVS-3-6", "NGVS+5+2","NGVS+5+1", "NGVS+5+0", "NGVS+5-1", "NGVS+0+6", "NGVS-6+1", "NGVS-6+0"]:
     start_time_one_tile = time.time()
     print("tile_id", tile_id)
-    tile_name = tile_id.replace('-', 'm').replace('+', 'p')
+    tile_name = tile_id.replace('m', "-").replace('p', "+")
+    print(tile_name)
  # Set the base destination directory
-    base_destination_dir = '/path/to/code/tiles'
+    base_destination_dir = '/DATA1/sdey/BlueBlobs_Virgo/code/tile/'
 
     # Create a new directory with the name of the tile ID in the base destination directory
-    destination_dir = os.path.join(base_destination_dir, tile_name)
+    destination_dir = os.path.join(base_destination_dir, tile_id)
     os.makedirs(destination_dir, exist_ok=True)
-    for file_type in []: ## "filter name egs. G, U, I2, Z etc.
+    for file_type in ["I2"]:#, "G", "U"]:
         start_time_one_file = time.time()
     # Construct the full URL by concatenating the base URL, the tile ID, and the file type
-        url = base_url + tile_id + '.' + file_type + '.fits'
-
+        url = base_url + tile_name + '.' + file_type + '.fits'
+        print(url)
     # Send a GET request to the download URL to download the file
         response = requests.get(url)
        # If the request was successful, save the file to the specified destination directory
@@ -28,7 +29,7 @@ for tile_id in []:##"NGVS+0-7", "NGVS-1-7","NGVS-2-7", "NGVS+1-6", "NGVS+0-6", "
 
 
                 # Join the destination directory with the file name
-            file_path = os.path.join(destination_dir, tile_id + '.' + file_type + '.fits')
+            file_path = os.path.join(destination_dir, tile_name + '.' + file_type + '.fits')
 
             with open(file_path, 'wb') as f:
                 f.write(response.content)
@@ -40,3 +41,4 @@ for tile_id in []:##"NGVS+0-7", "NGVS-1-7","NGVS-2-7", "NGVS+1-6", "NGVS+0-6", "
               end_time_one_file - start_time_one_file)
     end_time_one_tile = time.time()
     print("Time_taken_one_tile =", end_time_one_tile - start_time_one_tile, "sec") 
+    
